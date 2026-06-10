dbaa576f27557d24ca428fa63d9518b9d3e3828a
Zqarebaqi <zqarebaqi@gmail.com>
2026-06-10 03:56:01 +0330

    add tailwindcss to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-
+import './styles/tailwind.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
